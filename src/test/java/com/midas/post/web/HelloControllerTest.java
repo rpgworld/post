710b61d640301ff468958b412d8947c1f3e9008e
rpgworld <cloud_data@naver.com>
2021-08-25 12:49:03 +0900

    Add HelloControllerDto & Test

--- src/test/java/com/midas/post/web/HelloControllerTest.java
+++ src/test/java/com/midas/post/web/HelloControllerTest.java
@@ -6,11 +6,11 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import static org.hamcrest.Matchers.is;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 //@SpringBootTest
 @WebMvcTest(controllers = HelloController.class)
@@ -29,5 +29,22 @@ public class HelloControllerTest {
         mvc.perform(get("/hello"))
                 .andExpect(status().isOk())
                 .andExpect(content().string(hello));
+    }
+
+    @Test
+    public void helloDto가_리턴된다() throws Exception {
+        // given
+        String name = "hello";
+        int amount = 1000;
+
+        // when
+        // then
+        mvc.perform(get("/hello/dto")
+                .param("name", name)
+                .param("amount", String.valueOf(amount)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name", is(name)))
+                .andExpect(jsonPath("$.amount", is(amount)));
+
     }
 }
---
+++ src/test/java/com/midas/post/web/dto/HelloResponseDtoTest.java
@@ -0,0 +1,25 @@
+package com.midas.post.web.dto;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class HelloResponseDtoTest {
+
+    @Test
+    public void 롬복기능테스트() throws Exception {
+        // given
+        String name = "test";
+        int amount = 1000;
+
+        // when
+        HelloResponseDto dto = new HelloResponseDto(name, amount);
+
+        // then
+        assertThat(dto.getName()).isEqualTo(name);
+        assertThat(dto.getAmount()).isEqualTo(amount);
+
+    }
+}
